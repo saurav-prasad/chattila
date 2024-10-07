f05c83737cec94e10bca8b8eed87c8dd196128bd
saurav-prasad <sauravprasad2050@gmail.com>
2024-10-07 14:02:45 +0530

    testing

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = process.env.REACT_APP_VERCEL_SERVER_URL
+const url = process.env.REACT_APP_SERVER_URL
 const url1 = 'http://localhost:5500'
 console.log(url);
 
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client'
 
 const ur = 'http://localhost:8080'
-const url = process.env.REACT_APP_VERCEL_SOCKET_URL
-
+const url = process.env.REACT_APP_SOCKET_URL
+console.log(url);
 const socket = io(url)
 
 export default socket
