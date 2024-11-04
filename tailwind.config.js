b3d976093f3636b61a76e7de20c80b02962e8c17
saurav-prasad <sauravprasad2050@gmail.com>
2024-11-04 19:10:06 +0530

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-})
+}
